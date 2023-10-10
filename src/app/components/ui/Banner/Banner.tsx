69cc7d473f2dff3f2da609dc8489ffce9d7c80b0
Gamza <gamza-ns@mail.ru>
2023-10-10 09:13:31 +0300

    feat: banner mobile

--- src/app/components/ui/Banner/Banner.tsx
+++ src/app/components/ui/Banner/Banner.tsx
@@ -3,16 +3,34 @@ import Image from 'next/image';
 export default function Banner(props: any) {
   const { head, text, imgUrl, bgColor } = props;
   return (
-    <div className={bgColor === 'blue' ? style.banner_blue : style.banner}>
+    <div
+      className={
+        bgColor === 'blue'
+          ? style.banner_blue
+          : bgColor === 'green'
+          ? style.banner_green
+          : style.banner
+      }
+    >
       <div className={style.banner__wrap}>
         <div className={style.banner__info}>
           <h2 className={style.banner__info__head}>{head}</h2>
           <p className={style.banner__info__text}>{text}</p>
         </div>
-        <button className={style.banner__btn}>Подобрать психолога</button>
+        <button
+          className={
+            bgColor === 'blue'
+              ? 'button_white_blue'
+              : bgColor === 'green'
+              ? 'button_white_green'
+              : 'etc'
+          }
+        >
+          Подобрать психолога
+        </button>
       </div>
 
-      <div>
+      <div className={style.banner__img}>
         <Image src={imgUrl} alt={imgUrl} width="334" height="393" />
       </div>
     </div>
