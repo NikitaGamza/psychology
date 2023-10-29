3ba849030a782abb9f89a42662ab9e6addf6bf51
Gamza <gamza-ns@mail.ru>
2023-10-29 21:40:30 +0300

    feat: team started

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import style from './Team.module.scss';
 
 export default function Team() {
   return (
-    <div>
-      <h1>This is team</h1>
+    <div className={style.about}>
+      <div className="container">
+        <div className={style.about__main}>
+          <div className={style.about__main__content}>
+            <h1>Подобрать психолога</h1>
+          </div>
+
+          <div className={style.about__main__parameters}>
+            <h5>параметры</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
