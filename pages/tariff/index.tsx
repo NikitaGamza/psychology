d615649a9f28aeed9afa84f71ec8793f176da79c
Gamza <gamza-ns@mail.ru>
2023-11-15 18:40:39 +0300

    feat: tariff page

---
+++ pages/tariff/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './style/Tariff.module.scss';
+import { tariffFilter } from './tariffFilter';
+import TariffItem from '@/app/components/pages/tariff/TariffItem/TariffItem';
+
+export default function Tariff() {
+  return (
+    <div className={style.tariff}>
+      <div className="container">
+        <h1 className={style.tariff__head}>Тарифы</h1>
+        <div className={style.tariff__list}>
+          {tariffFilter.map((item) => (
+            <TariffItem tariffFilter={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/pages/tariff/TariffItem/TariffItem.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import style from './Tariff.module.scss';
+import Tariff from '@/app/components/ui/Tariff/Tariff';
+import { tariffList } from './tariffList';
+
+export default function TariffItem(tariffFilter: any) {
+  const { therapyName, format } = tariffFilter.tariffFilter;
+  const [tariffFormat, setTariffFormat] = useState(format[0]);
+  useEffect(() => {
+    console.log(tariffFilter);
+  });
+  return (
+    <div className={style.tariff}>
+      <div className={style.tariff__info}>
+        <h3 className={style.tariff__info__head}>{therapyName}</h3>
+        <div className={style.tariff__info__btns}>
+          {format.map((item: string, idx: number) => (
+            <button
+              key={idx}
+              className={
+                item === tariffFormat
+                  ? style.tariff__info__btns__item_active
+                  : style.tariff__info__btns__item
+              }
+              onClick={() => setTariffFormat(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+        <ul className={style.tariff__info__bullet}>
+          <li className={style.tariff__info__bullet__item}>
+            Видео- или аудиоконсультаций на 50 минут
+          </li>
+          <li className={style.tariff__info__bullet__item}>
+            Общение в Zoom или Skype
+          </li>
+        </ul>
+        <button className={style.tariff__info__req}>Оставить заявку</button>
+      </div>
+      <div className={style.tariff__carts}>
+        {tariffList.map(
+          (item: any, idx: number) =>
+            item.format === tariffFormat &&
+            item.tariffPlan === therapyName && (
+              <Tariff
+                key={idx}
+                personal={item.personal}
+                type={item.format}
+                price={item.price}
+                priceOne={item.priceOne}
+                quantity={item.quantity}
+                sale={item.sale}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
