7c08b2342a1be56609e9656ac565eecd6d69404c
Gamza <gamza-ns@mail.ru>
2023-11-16 02:04:01 +0300

    feat: FAQ

---
+++ pages/faq/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './style/Faq.module.scss';
+
+export default function Faq() {
+  return (
+    <div className={style.faq}>
+      <h1>FAQ</h1>
+    </div>
+  );
+}
