89eb11ef6aa07174200c40d48ef07ef6102692e4
Gamza <gamza-ns@mail.ru>
2023-11-16 10:10:31 +0300

    feat: faq desktop

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import style from './style/Faq.module.scss';
+import { faqList } from './faqList';
+import Item from '@/app/components/pages/faq/Item/Item';
 
 export default function Faq() {
   return (
     <div className={style.faq}>
-      <h1>FAQ</h1>
+      <div className="container">
+        <h2 className={style.faq__head}>Ваши вопросы</h2>
+        <div className={style.faq__list}>
+          {faqList.map((item: any, idx: number) => (
+            <Item key={idx} props={item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/components/pages/faq/Item/Item.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import style from './Item.module.scss';
+
+export default function Item({ props }: any) {
+  const { head, description } = props;
+  const [visible, setVisible] = useState<boolean>(false);
+  return (
+    <div className={style.item}>
+      <div className={style.item__head}>
+        <span className={style.item__head__title}>{head}</span>
+        <button
+          onClick={() => setVisible(!visible)}
+          className={
+            visible
+              ? style.item__head__btn_visible
+              : style.item__head__btn_invisible
+          }
+        >
+          {visible ? '-' : '+'}
+        </button>
+      </div>
+      {visible && <p className={style.item__description}>{description}</p>}
+    </div>
+  );
+}
