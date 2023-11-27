3c5b3ffc64e716315628ab35db1184691619e95b
Gamza <gamza-ns@mail.ru>
2023-11-27 07:07:05 +0300

    fix: tariff ellipse

--- src/app/components/ui/Tariff/Tariff.tsx
+++ src/app/components/ui/Tariff/Tariff.tsx
@@ -3,7 +3,19 @@ import style from './Tariff.module.scss';
 export default function Tariff(props: any) {
   const { personal, type, quantity, priceOne, price, sale } = props;
   return (
-    <div className={style.tariff__cart}>
+    <div
+      className={`${style.tariff__cart}
+        ${
+          quantity === 1
+            ? style.tariff__cart_1
+            : quantity === 4
+            ? style.tariff__cart_4
+            : quantity === 8
+            ? style.tariff__cart_8
+            : style.tariff__cart_12
+        }
+      `}
+    >
       {sale && (
         <div className={style.tariff__cart__sale}>
           <span className={style.tariff__cart__sale__info}>-{sale}%</span>
