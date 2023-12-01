e7084e8919c104e691fef10c18e6f14ee33562f6
Gamza <gamza-ns@mail.ru>
2023-12-01 22:24:47 +0300

    init: created payment page

---
+++ pages/payment/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Payment.module.scss';
+
+export default function Payment() {
+  return (
+    <div className={style.pay}>
+      <div className="container">
+        <h1>Payment</h1>
+      </div>
+    </div>
+  );
+}
