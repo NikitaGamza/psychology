c8f5ae54d3c77820f170f0ebe0682ebd574c29da
Gamza <gamza-ns@mail.ru>
2023-12-02 03:13:10 +0300

    feat: payment general info component

--- pages/payment/index.tsx
+++ pages/payment/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import style from './Payment.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import General from '@/app/components/pages/payment/General/General';
 
 export default function Payment() {
   return (
     <div className={style.pay}>
       <div className="container">
-        <h1>Payment</h1>
+        <Link href={'/docs'} className={style.pay__back}>
+          <Image
+            src={'/img/icons/arrows/arrow-left-green.svg'}
+            alt="back"
+            width={10}
+            height={7}
+            className={style.pay__back__arrow}
+          />
+          Назад
+        </Link>
+        <div className={style.pay__content}>
+          <h2 className={style.pay__content__head}>Способы оплаты</h2>
+          <div className={style.pay__content__info}>
+            <General />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/app/components/pages/payment/General/General.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import style from './General.module.scss';
+import Link from 'next/link';
+
+export default function General() {
+  return (
+    <div className={style.gen}>
+      <h4 className={style.gen__head}>Оплата</h4>
+      <div className={style.gen__info}>
+        <p className={style.gen__info__text}>
+          Мы принимаем оплаты из любой точки мира.
+        </p>
+        <ol className={style.gen__info__ol}>
+          <li className={style.gen__info__ol__li}>
+            <Link href={'/'} className={style.gen__info__ol__li_link}>
+              Заполните анкету
+            </Link>{' '}
+            на подбор психолога у нас на сайте
+          </li>
+          <li className={style.gen__info__ol__li}>
+            Специалист по подбору психолога напишет вам в WhatsApp, уточнит
+            запрос и пришлёт ссылку на оплату в чат
+          </li>
+          <li className={style.gen__info__ol__li}>
+            После оплаты вам подберут психолога
+          </li>
+        </ol>
+        <p className={style.gen__info__text}>
+          Если вы выбираете психолога самостоятельно в каталоге, специалист
+          нашего сервиса пришлёт в чат ссылку на оплату после того, как вы
+          определитесь с психологом и типом консультаций.
+        </p>
+      </div>
+    </div>
+  );
+}
