690576bb8c79bfdf690443ef71be89efe24f9b18
Gamza <gamza-ns@mail.ru>
2023-12-03 00:23:54 +0300

    init: courses page

---
+++ pages/courses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Courses.module.scss';
+
+export default function Courses() {
+  return (
+    <div className={style.course}>
+      <div className="container">
+        <h1>Курсы</h1>
+      </div>
+    </div>
+  );
+}
