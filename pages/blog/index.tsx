a0a0d63f47131ca1c003b8211d0eec6bb8a134ca
Gamza <gamza-ns@mail.ru>
2023-12-12 11:36:17 +0300

    init: blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Blog.module.scss';
+
+export default function Blog() {
+  return (
+    <div className={style.blog}>
+      <h1>blog</h1>
+    </div>
+  );
+}
