e89f9d193a6ebce80a0d2f96cea69b9ca1ef96c6
Gamza <gamza-ns@mail.ru>
2023-12-14 11:37:20 +0300

    feat: question page

--- pages/blog/questions/index.tsx
+++ pages/blog/questions/index.tsx
@@ -1,12 +1,143 @@
 import React from 'react';
 import style from './Questions.module.scss';
 import BlogLayout from '../layout';
+import Link from 'next/link';
 
 export default function Questions() {
+  const themesList = [
+    'Дети',
+    'Семья',
+    'Личностный рост',
+    'Самоопределение',
+    'Карьера',
+    'Депрессия',
+    'Грусть, тоска',
+    'Тревога',
+    'Прокрастинация',
+    'Апатия',
+    'Панические атаки',
+    'Травмы',
+    'Неуверенность в себе',
+    'Отношения',
+    'Выгорание',
+  ];
+  const questionList = [
+    {
+      id: 1,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как правильно налаживать свою жизнь?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выбopa шкaфoв, тoгo, c кeм вы пpoвoдитe cвoё вpeмя, кaкиe пoдapки дapитe, вo чтo вepитe и нa кaких мыcлях фoкycиpyeтecь. Хaoc — этo выбop. Гнeв — этo выбop. Для мужчины главное понимать, что ты его любишь и что ты ему верна. Чeгo бы oн ни кaсался',
+    },
+    {
+      id: 2,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как пережить горе и вернуться к жизне?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
+    },
+    {
+      id: 3,
+      themes: ['Отношения', 'Семья'],
+      head: 'Любовь и деньги',
+      answer:
+        'Здравствуйте! Я первый раз пишу на этом сервисе. Обидa — тoжe выбop. Тaк жe кaк и шиpoтa дyши, гибкocть, cocтpaдaниe, нeжнocть. Обидa — тoжe',
+    },
+    {
+      id: 4,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как пережить горе и вернуться к жизне?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
+    },
+    {
+      id: 5,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как укрепить свою психику?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
+    },
+    {
+      id: 6,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как пережить горе и вернуться к жизне?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
+    },
+    {
+      id: 7,
+      themes: ['Отношения', 'Семья'],
+      head: 'Как правильно налаживать свою жизнь?',
+      answer:
+        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выбopa шкaфoв, тoгo, c кeм вы пpoвoдитe cвoё вpeмя, кaкиe пoдapки дapитe, вo чтo вepитe и нa кaких мыcлях фoкycиpyeтecь. Хaoc — этo выбop. Гнeв — этo выбop. Для мужчины главное понимать, что ты его любишь и что ты ему верна. Чeгo бы oн ни кaсался',
+    },
+  ];
+  interface ICart {
+    id: number;
+    themes: Array<string>;
+    head: string;
+    answer: string;
+  }
   return (
     <BlogLayout>
-      <div>
-        <div></div>
+      <div className={style.que}>
+        <h1 className={style.que__head}>Вопросы</h1>
+        <div className={style.que__order}>
+          <button className={style.que__order__btn}>Самые читаемые</button>
+          <button className={style.que__order__btn}>По дате публикации</button>
+          <button className={style.que__order__btn}>По теме</button>
+        </div>
+        <div className={style.que__themes}>
+          {themesList.map((item: string, idx: number) => (
+            <button key={idx} className={style.que__themes__btn}>
+              {item}
+            </button>
+          ))}
+        </div>
+        <div className={style.que__content}>
+          {questionList.map((item: ICart) => (
+            <div
+              key={item.id}
+              className={
+                item.id === 1
+                  ? style.que__content__item_1
+                  : item.id === 2
+                  ? style.que__content__item_2
+                  : item.id === 3
+                  ? style.que__content__item_3
+                  : item.id === 4
+                  ? style.que__content__item_4
+                  : item.id === 5
+                  ? style.que__content__item_5
+                  : item.id === 6
+                  ? style.que__content__item_6
+                  : style.que__content__item_7
+              }
+            >
+              <div className={style.que__content__item__themes}>
+                {item.themes.map((theme: string, idx: number) => (
+                  <span
+                    key={idx}
+                    className={style.que__content__item__themes__item}
+                  >
+                    {theme}
+                  </span>
+                ))}
+              </div>
+              <h4 className={style.que__content__item__head}>{item.head}</h4>
+              <p className={style.que__content__item__answer}>
+                {item.answer}...{' '}
+              </p>
+              <Link
+                className={style.que__content__item__link}
+                href={`/blog/questions/${item.id}`}
+              >
+                Читать далее
+              </Link>{' '}
+            </div>
+          ))}
+        </div>
+        <button className={style.que__more}>Показать еще</button>
       </div>
     </BlogLayout>
   );
