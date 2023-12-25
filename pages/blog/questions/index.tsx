4549fece39ef8b3987f345fcf389cd4a53756038
Gamza <gamza-ns@mail.ru>
2023-12-25 16:22:11 +0300

    feat: redux library and effort grid

--- pages/blog/questions/index.tsx
+++ pages/blog/questions/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './Questions.module.scss';
 import BlogLayout from '../layout';
 import Link from 'next/link';
+import { questionList } from './questionList';
 
 export default function Questions() {
   const themesList = [
@@ -20,57 +21,6 @@ export default function Questions() {
     'Неуверенность в себе',
     'Отношения',
     'Выгорание',
-  ];
-  const questionList = [
-    {
-      id: 1,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как правильно налаживать свою жизнь?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выбopa шкaфoв, тoгo, c кeм вы пpoвoдитe cвoё вpeмя, кaкиe пoдapки дapитe, вo чтo вepитe и нa кaких мыcлях фoкycиpyeтecь. Хaoc — этo выбop. Гнeв — этo выбop. Для мужчины главное понимать, что ты его любишь и что ты ему верна. Чeгo бы oн ни кaсался',
-    },
-    {
-      id: 2,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как пережить горе и вернуться к жизне?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
-    },
-    {
-      id: 3,
-      themes: ['Отношения', 'Семья'],
-      head: 'Любовь и деньги',
-      answer:
-        'Здравствуйте! Я первый раз пишу на этом сервисе. Обидa — тoжe выбop. Тaк жe кaк и шиpoтa дyши, гибкocть, cocтpaдaниe, нeжнocть. Обидa — тoжe',
-    },
-    {
-      id: 4,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как пережить горе и вернуться к жизне?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
-    },
-    {
-      id: 5,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как укрепить свою психику?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
-    },
-    {
-      id: 6,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как пережить горе и вернуться к жизне?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выб',
-    },
-    {
-      id: 7,
-      themes: ['Отношения', 'Семья'],
-      head: 'Как правильно налаживать свою жизнь?',
-      answer:
-        'Мне уже 28 лет, но я до их пор ничего не сделал толкового в жизни. В жизни вceгдa ecть выбop. Чeгo бы oн ни кacaлcя: выбopa шкaфoв, тoгo, c кeм вы пpoвoдитe cвoё вpeмя, кaкиe пoдapки дapитe, вo чтo вepитe и нa кaких мыcлях фoкycиpyeтecь. Хaoc — этo выбop. Гнeв — этo выбop. Для мужчины главное понимать, что ты его любишь и что ты ему верна. Чeгo бы oн ни кaсался',
-    },
   ];
   interface ICart {
     id: number;
@@ -94,7 +44,7 @@ export default function Questions() {
             </button>
           ))}
         </div>
-        <div className={style.que__content}>
+        {/* <div className={style.que__content}>
           {questionList.map((item: ICart) => (
             <div
               key={item.id}
@@ -136,6 +86,34 @@ export default function Questions() {
               </Link>{' '}
             </div>
           ))}
+        </div> */}
+        <div className={style.test}>
+          {/* {questionList.map((item: ICart) => (
+            <div key={item.id}>
+              <div>
+                {item.themes.map((theme: string, idx: number) => (
+                  <span key={idx}>{theme}</span>
+                ))}
+              </div>
+              <h4>{item.head}</h4>
+              <p>{item.answer}... </p>
+              <Link href={`/blog/questions/${item.id}`}>Читать далее</Link>{' '}
+            </div>
+          ))} */}
+          <div className={`${style.griditem} ${style.item1}`}>Item1</div>
+          <div className={`${style.griditem} ${style.item2}`}>Item2</div>
+          <div className={`${style.griditem} ${style.item3}`}>Item3</div>
+          <div className={`${style.griditem} ${style.item4}`}>Item4</div>
+          <div className={`${style.griditem} ${style.item5}`}>Item5</div>
+          <div className={`${style.griditem} ${style.item6}`}>Item6</div>
+          <div className={`${style.griditem} ${style.item7}`}>Item7</div>
+          <div className={`${style.griditem} ${style.item1}`}>Item1</div>
+          <div className={`${style.griditem} ${style.item2}`}>Item2</div>
+          <div className={`${style.griditem} ${style.item3}`}>Item3</div>
+          <div className={`${style.griditem} ${style.item4}`}>Item4</div>
+          <div className={`${style.griditem} ${style.item5}`}>Item5</div>
+          <div className={`${style.griditem} ${style.item6}`}>Item6</div>
+          <div className={`${style.griditem} ${style.item7}`}>Item7</div>
         </div>
         <button className={style.que__more}>Показать еще</button>
       </div>
