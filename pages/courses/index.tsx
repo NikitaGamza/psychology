8a8c7d3564a3d49c99531735086adbfb63144305
Gamza <gamza-ns@mail.ru>
2024-01-17 18:10:13 +0300

    try to fix courses 2

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import style from './Courses.module.scss';
-import Main from '@/app/components/pages/courses/Main/Main';
+import React from 'react';
+// import style from './Courses.module.scss';
+// import Main from '@/app/components/pages/courses/Main/Main';
 // import Filters from '@/app/components/pages/courses/Filters/Filters';
-import Image from 'next/image';
-import MobileFilter from '@/app/components/pages/courses/MobileFilter/MobileFilter';
+// import Image from 'next/image';
+// import MobileFilter from '@/app/components/pages/courses/MobileFilter/MobileFilter';
 
 export default function Courses() {
-  const [mobileFilt, setMobileFilt] = useState<boolean>(false);
+  // const [mobileFilt, setMobileFilt] = useState<boolean>(false);
   return (
-    <div className={style.course}>
-      {mobileFilt && (
+    <div>
+      {/* {mobileFilt && (
         <MobileFilter setMobileFilt={setMobileFilt} mobileFilt={mobileFilt} />
-      )}
-      <div className="container">
+      )} */}
+      {/* <div className="container">
         <h2 className={style.course__head}>Курсы</h2>
         <div className={style.course__head_mob}>
           <h2 className={style.course__head_mob__title}>Курсы</h2>
@@ -24,11 +24,11 @@ export default function Courses() {
             onClick={() => setMobileFilt(!mobileFilt)}
           />
         </div>
-        <div className={style.course__content}>
-          <Main />
-          {/* <Filters /> */}
-        </div>
-      </div>
+        <div className={style.course__content}> */}
+      {/* <Main /> */}
+      {/* <Filters /> */}
+      {/* </div>
+      </div> */}
     </div>
   );
 }
