a3a5e963d5e32ac40a5dbffaf8ce08b74cf87cb8
Vladimir Minkin <vladimir.minkin@gmail.com>
2024-04-18 17:32:39 +0000

    change pm2 config to run project as static web server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   apps: [{
-    script: 'yarn start',
-    name: 'psychology'
+    script: 'serve',
+    name: 'psychology',
+    env: {
+      PM2_SERVE_PATH: '.out',
+      PM2_SERVE_PORT: 8080,
+      PM2_SERVE_SPA: 'true',
+      PM2_SERVE_HOMEPAGE: './index.html'
+    }
   }],
 };
