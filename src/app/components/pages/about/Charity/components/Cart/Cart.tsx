d2848c5978c58a030a76bb5307851e796c5c0d12
Gamza <gamza-ns@mail.ru>
2024-05-07 19:41:16 +0300

    fix: charity slider

--- src/app/components/pages/about/Charity/components/Cart/Cart.tsx
+++ src/app/components/pages/about/Charity/components/Cart/Cart.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Cart(props: any) {
   const { imgUrl, head, text } = props.props;
   return (
-    <div className={`keen-slider__slide ${style.cart}`}>
+    <div className={`${style.cart}`}>
       <Image
         src={imgUrl}
         width={20}
